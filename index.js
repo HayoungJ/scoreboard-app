52225a2126d6f3d37fa4f642c5fa36d715bdee25
Hayoung Jeon <hayoungj0710@gmail.com>
2023-01-01 22:46:39 +0900

    ADD: 초기화면을 ScoreScreen으로 연결

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import ScoreScreen from './app/screens/ScoreScreen';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => ScoreScreen);
